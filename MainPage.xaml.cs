1963a685d2f4b6586230fe2cdd1e8616269edac9
ay27 <me@ay27.pw>
2013-12-15 09:34:23 +0800

    First commit

---
+++ MainPage.xaml.cs
@@ -0,0 +1,207 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using System.Text;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.System;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+using NumberSystem;
+using Windows.UI.Popups;
+using Windows.UI;
+using Windows.UI.ApplicationSettings;
+
+namespace ProgramerCalc
+{
+
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+            SettingsPane.GetForCurrentView().CommandsRequested += App_CommandsRequested;
+        }
+
+        private Color _background = Color.FromArgb(255, 0, 77, 96);
+        private void App_CommandsRequested(SettingsPane sender, SettingsPaneCommandsRequestedEventArgs args)
+        {
+            var about = new SettingsCommand("about", "About", (handler) =>
+                {
+                SettingsFlyout settings = new SettingsFlyout();
+                settings.Content = new AboutUserControl();
+                //settings.Background = new SolidColorBrush(_background);
+                //settings.HeaderBackground = new SolidColorBrush(_background);
+                //settings.Title = "About";
+                settings.Show();
+            });
+            args.Request.ApplicationCommands.Add(about);
+        }
+
+        private void textBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            TextBox box = sender as TextBox;
+            StringBuilder sb = new StringBuilder(box.Text.ToUpper());
+            sb.Replace("XOR", "xor");
+            foreach (VirtualKey key in disableKey)
+            {
+                int index = sb.ToString().IndexOf(key.ToString()[key.ToString().Length-1]);
+                if (index == -1) continue;
+                sb.Remove(index, 1);
+            }
+            box.Text = sb.ToString();
+            box.Select(box.Text.Length, 0);
+        }
+
+        private void textBox_KeyDown(object sender, KeyRoutedEventArgs e)
+        {
+            if (e.Key == VirtualKey.Enter)
+            {
+                calc();
+                e.Handled = true;
+            }
+            if (e.Key > VirtualKey.F && e.Key <= VirtualKey.Z)
+                e.Handled = true;
+            else if (disableKey.IndexOf(e.Key) != -1)
+                e.Handled = true;
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Button button = sender as Button;
+            String text = (String)button.Content;
+            TextBox_AddText(this.textBox, text);
+        }
+
+        private void TextBox_AddText(TextBox box, String text)
+        {
+            box.Text += text;
+            box.Select(box.Text.Length, 0);
+        }
+
+        private List<VirtualKey> disableKey = new List<VirtualKey>();
+        private int selected;
+        private void ComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            disableKey.Clear();
+            selected = (sender as ComboBox).SelectedIndex;
+            if (selected >= 1)
+            {
+                disableKey.Add(VirtualKey.A);
+                disableKey.Add(VirtualKey.B);
+                disableKey.Add(VirtualKey.C);
+                disableKey.Add(VirtualKey.D);
+                disableKey.Add(VirtualKey.E);
+                disableKey.Add(VirtualKey.F);
+            }
+            if (selected >= 2)
+            {
+                disableKey.Add(VirtualKey.Number9);
+                disableKey.Add(VirtualKey.Number8);
+                disableKey.Add(VirtualKey.NumberPad9);
+                disableKey.Add(VirtualKey.NumberPad8);
+            }
+            if (selected == 3)
+            {
+                disableKey.Add(VirtualKey.Number7);
+                disableKey.Add(VirtualKey.Number6);
+                disableKey.Add(VirtualKey.Number5);
+                disableKey.Add(VirtualKey.Number4);
+                disableKey.Add(VirtualKey.Number3);
+                disableKey.Add(VirtualKey.Number2);
+                disableKey.Add(VirtualKey.NumberPad7);
+                disableKey.Add(VirtualKey.NumberPad6);
+                disableKey.Add(VirtualKey.NumberPad5);
+                disableKey.Add(VirtualKey.NumberPad4);
+                disableKey.Add(VirtualKey.NumberPad3);
+                disableKey.Add(VirtualKey.NumberPad2);
+            }
+
+        }
+
+        private async void calc()
+        {
+            ClearValueInTextBox();
+
+            int di = 10;
+            if (selected == 0)
+                di = 16;
+            else if (selected == 1)
+                di = 10;
+            else if (selected == 2)
+                di = 8;
+            else if (selected == 3)
+                di = 2;
+            Parser parser = new Parser(this.textBox.Text, di);
+
+            String falseStr = null;
+            Int64 result = 0;
+            try
+            {
+                result = (Int64)parser.parse();     // 返回十进制
+
+                if (result < 0)
+                {
+                    throw (new Exception("The result is lower than ZERO."));
+                }
+
+                TextBox_AddText(this.textBox, "=");
+                TextBox_AddText(this.textBox, "" + (new Number(result)).getX(di));
+                TextBox_AddText(this.textBox2, "" + (new Number(result)).getX(2));
+                TextBox_AddText(this.textBox8, "" + (new Number(result)).getX(8));
+                TextBox_AddText(this.textBox10, "" + (new Number(result)).getX(10));
+                TextBox_AddText(this.textBox16, "" + (new Number(result)).getX(16));
+
+            }
+            catch (Exception exception)
+            {
+                falseStr = exception.Message;
+            }
+            if (falseStr != null)
+            {
+                var dialog = new MessageDialog(falseStr);
+                await dialog.ShowAsync();
+            }
+        }
+
+        private void Button_Click_result(object sender, RoutedEventArgs e)
+        {
+            calc();
+        }
+
+        private void Button_Clear_Click(object sender, RoutedEventArgs e)
+        {
+            textBox.Text = "";
+            textBox.Select(0, 0);
+            ClearValueInTextBox();
+        }
+
+
+        private static readonly String text2 = "2进制：";
+        private static readonly String text8 = "8进制：";
+        private static readonly String text10 = "10进制：";
+        private static readonly String text16 = "16进制：";
+        private void ClearValueInTextBox()
+        {
+            textBox2.Text = text2;
+            textBox2.Select(textBox2.Text.Length, 0);
+            textBox8.Text = text8;
+            textBox8.Select(textBox8.Text.Length, 0);
+            textBox10.Text = text10;
+            textBox10.Select(textBox10.Text.Length, 0);
+            textBox16.Text = text16;
+            textBox16.Select(textBox16.Text.Length, 0);
+        }
+
+
+
+    }
+}
---
+++ NumberSystem.cs
@@ -0,0 +1,357 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NumberSystem
+{
+
+    class Number
+    {
+        private String value;
+        // 10-base value
+        private Int64 _value;
+        private int digit;
+
+        public Number(String value, int digit)
+        {
+            if (digit > 36) throw (new Exception("the digit is too LARGE!"));
+
+            this.value = value;
+            this._value = changeTo10(value, digit);
+            this.digit = digit;
+        }
+
+        public Number(Int64 value)
+        {
+            this.digit = 10;
+            this._value = value;
+            this.value = changeFrom10(_value, 10);
+        }
+
+        public String getX(int di)
+        {
+            if (digit == di) return value;
+            Int64 _value10 = changeTo10(value, digit);
+            return changeFrom10(_value10, di);
+        }
+
+
+
+        /** change a di-base String to a 10-base int
+         * such as:
+         * 1100(2)->12(10)
+         * 
+         */
+        public static Int64 changeTo10(String value, int di)
+        {
+            Int64 tt = 1;
+            Int64 result = 0;
+            int k;
+            for (int i = value.Length - 1; i >= 0; i--)
+            {
+                k = isAlpha(value[i]) ? toLowerCase(value[i]) - 'a' + 10 : (value[i] - '0');
+                result += tt * k;
+                tt *= di;
+            }
+
+            return result;
+        }
+        
+        /**
+         * change a 10-base number to a destDi-base String 
+         * such as:
+         * 31(10)->1F(16)
+         * 
+         */
+        public static String changeFrom10(Int64 k, int destDi)
+        {
+            StringBuilder sb = new StringBuilder();
+            int t;
+            while (k>0)
+            {
+                t = (int)(k % destDi);
+                sb.Insert(0, t > 9 ? (char)(t - 10 + 'A') : (char)(t+'0'));
+                k /= destDi;
+            }
+            return sb.ToString();
+        }
+
+
+
+        private static bool isAlpha(char ch)
+        {
+            return (ch >= 'a' && ch <= 'z') || (ch >= 'A' && ch <= 'Z');
+        }
+
+        private static char toLowerCase(char ch)
+        {
+            return (ch >= 'A' && ch <= 'Z') ? (char)(ch - 'A' + 'a') : ch;
+        }
+    }
+
+    class Parser
+    {
+        //表达式的结尾标记符
+        private readonly string EOE = "\0";
+
+        private string exp;  //保存表达式的一个副本
+        private string token;  //保存当前待处理的字符串
+        private int exp_id;   //保存当前处理的位置
+        private int di;       // 保存当前字符串是几进制的
+
+        public Parser(String str, int di)
+        {
+            this.di = di;
+            exp_id = 0;
+            exp = str;
+        }
+
+        public Int64 parse()//解析表达式的入口，同时初始化类中的变量 
+        {
+            get_token();
+
+            return work0();
+        }
+
+        private Int64 work0()
+        {
+            Int64 result1, result2;
+            
+            result1 = work1();
+            String op = token;
+
+            while (op == "|")
+            {
+                get_token();
+                result2 = work1();
+                result1 |= result2;
+                op = token;
+            }
+            return result1;
+        }
+
+        private Int64 work1()
+        {
+            Int64 result1, result2;
+            
+            result1 = work2();
+            String op = token;
+
+            while (op == "xor")
+            {
+                get_token();
+                result2 = work2();
+                result1 = result1 ^ result2;
+                op = token;
+            }
+            return result1;
+        }
+
+        private Int64 work2()
+        {
+            Int64 result1, result2;
+            result1 = work3();
+            String op = token;
+
+            while (op == "&")
+            {
+                get_token();
+                result2 = work3();
+                result1 &= result2;
+                op = token;
+            }
+            return result1;
+        }
+
+        private Int64 work3()   // 处理左移和右移运算符
+        {
+            Int64 result1, result2;
+            result1 = work4();
+
+            String op = token;
+
+            while (op == "<<" || op == ">>")
+            {
+                get_token();
+                result2 = work4();
+                if (op == "<<")
+                    result1 = (int)result1 << (int)result2;
+                else 
+                    result1 = (int)result1 >> (int)result2;
+                op = token;
+            }
+            return result1;
+        }
+
+        private Int64 work4()//处理加法和减法
+        {
+            char op;
+            Int64 result1, result2;
+
+            result1 = work5();
+
+            while ((op = token[0]) == '+' || op == '-')
+            {
+                get_token();
+                result2 = work5();
+                if (op == '+')
+                    result1 += result2;
+                else if (op == '-')
+                    result1 -= result2;
+            }
+            return result1;
+        }
+
+        private Int64 work5()//处理乘法除法和求余
+        {
+            char op;
+            Int64 result1, result2;
+
+            result1 = work6();
+
+            while ((op = token[0]) == '*' || op == '/')
+            {
+                get_token();
+                result2 = work6();
+
+                if (op == '*') result1 *= result2;
+                else result1 /= result2;
+            }
+
+            return result1;
+        }
+
+        private Int64 work6()//处理乘方
+        {
+            Int64 result1, ex, result2;
+
+            result1 = work7();
+
+            if (token == "^")
+            {
+                get_token();
+                result2 = work7();
+                ex = result1;
+
+                if (result2 == 0) result1 = 1;
+                else
+                    for (int i = 1; i < result2; i++)
+                        result1 *= ex;
+            }
+            return result1;
+        }
+
+        private Int64 work7() //处理正负号
+        {
+            Int64 result;
+            string op;
+
+            op = "";
+            if (token == "+" || token == "-" || token == "~")
+            {
+                op = token;
+                get_token();
+            }
+            result = work8();
+
+            if (op == "-") return -result;
+            else if (op == "~") return ~result;
+            else return result;
+        }
+
+        private Int64 work8() //处理括号
+        {
+            Int64 result;
+
+            if (token == "(")
+            {
+                get_token();
+                result = work0();
+                if (token != ")")
+                    throw (new Exception("右括号缺失"));
+            }
+            else result = work();
+
+            return result;
+        }
+
+        private Int64 work()//返回数字
+        {
+            Int64 result = Number.changeTo10(token, di);
+            get_token();
+            return result;
+        }
+        
+        private void get_token() //寻找下一组字符
+        {
+            token = "";
+
+            if (exp_id == exp.Length)
+            {
+                token = EOE;
+                return;
+            }
+
+            while (exp_id < exp.Length && exp[exp_id] == ' ')
+                exp_id++;
+
+            if (exp_id >= exp.Length)
+            {
+                token = EOE;
+                return;
+            }
+
+            if (isdelim(exp[exp_id]))
+            {
+                if ("<>".IndexOf(exp[exp_id]) != -1)
+                {
+                    token += exp[exp_id];
+                    exp_id++;
+                    if (exp[exp_id] != exp[exp_id - 1])
+                        throw (new Exception("左移/右移运算符不完整"));
+                    token += exp[exp_id];
+                    exp_id++;
+                }
+                else
+                {
+                    token += exp[exp_id];
+                    exp_id++;
+                }
+            }
+            else if (exp[exp_id] == 'x')
+            {
+                token += exp[exp_id++];
+                token += exp[exp_id++];
+                token += exp[exp_id++];
+                if (token != "xor")
+                    throw (new Exception("xor运算符不完整"));
+            }
+            else if (isDigitOrLetter(exp[exp_id]))
+            {
+                while (exp_id < exp.Length && isDigitOrLetter(exp[exp_id]))
+                {
+                    token += exp[exp_id];
+                    exp_id++;
+                }
+            }
+        }
+
+        private static bool isDigitOrLetter(char ch)
+        {
+            if (Char.IsDigit(ch))
+                return true;
+            if (ch >= 'A' && ch <= 'F')
+                return true;
+            return false;
+        }
+
+        private static bool isdelim(char c)  //判断是否为运算符
+        {
+            if ("/*-+()<>|%^&".IndexOf(c) != -1)
+                return true;
+            else return false;
+        }
+    };
+}
